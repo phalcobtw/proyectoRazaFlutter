7b74ccd721042134136c981a48d899e1b845f90f
phalcobtw <laxz_16@hotmail.com>
2022-09-29 10:50:52 -0600

    vista creandose y runapp inyectada

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,47 @@
+import 'package:dogapi/vistas/vista_creandose.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc.dart';
 
 void main() {
-  
+  runApp(AplicacionInyectada());
 }
 
+class AplicacionInyectada extends StatelessWidget {
+  const AplicacionInyectada({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) { 
+        ClaseBloc blocVerificacion = ClaseBloc();
+        Future.delayed(Duration(seconds: 2),(() {
+          blocVerificacion.add(Creado());
+        }));
+        return blocVerificacion;
+      },
+      child: Container(),
+    );
+  }
+}
+
+class Aplicacion extends StatelessWidget {
+  const Aplicacion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Builder(builder: (context){
+          var estado = context.watch<ClaseBloc>().state;
+          if (estado is Creandose) {
+            return VistaCreandose();
+          }
+          return const Center(child: Text('ERROR'),);
+        }),
+      ),
+    );
+  }
+}
---
+++ lib/vistas/vista_creandose.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class VistaCreandose extends StatelessWidget {
+  const VistaCreandose({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 100,
+        height: 100,
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
