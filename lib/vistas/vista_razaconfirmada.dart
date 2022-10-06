b2d5291c2397d82dcdfdfeb8471c0e5c9004196c
phalcobtw <laxz_16@hotmail.com>
2022-10-06 00:31:22 -0600

    vista mostrando la raza terminada

---
+++ lib/vistas/vista_razaconfirmada.dart
@@ -0,0 +1,28 @@
+import 'package:dogapi/raza_formada.dart';
+import 'package:dogapi/registro_raza.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc.dart';
+
+class VistaRazaConfirmada extends StatelessWidget {
+  const VistaRazaConfirmada({super.key, required this.registro, required this.raza});
+  final RegistroRaza registro;
+  final RazaFormada raza;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text('Nombre: ${raza.valor}'),
+          Text('SubRazas: ${registro.subraza}'),
+          TextButton(onPressed: (){
+            final Blocbtw = context.read<ClaseBloc>();
+            Blocbtw.add(Creado());
+          }, child: const Text('Regresar')),
+        ],
+      ),
+    );
+  }
+}
