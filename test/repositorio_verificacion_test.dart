cb7239152543fca404e520fa18c33b876b39ce67
phalcobtw <laxz_16@hotmail.com>
2022-10-05 23:13:13 -0600

    test solo raza, subraza y no existente

---
+++ test/repositorio_verificacion_test.dart
@@ -0,0 +1,33 @@
+import 'package:dogapi/problemas.dart';
+import 'package:dogapi/raza_formada.dart';
+import 'package:dogapi/repositorio_verificacion.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+ group('TEST raza, subrazas y no existentes', (){
+  RepositorioPruebaVerificacion repositorio = RepositorioPruebaVerificacion();
+  test('solo raza', () {
+    var resultado = repositorio.obtenerRegistroRaza(RazaFormada.constructor('Akita'));
+    resultado.match((l) {
+      expect(l, isA<sinSubRazas>());
+    }, ((r) {
+    }));
+  });
+  test('raza con subraza', () {
+    var resultado = repositorio.obtenerRegistroRaza(RazaFormada.constructor('hound'));
+    resultado.match((l) {
+
+    }, (r) {
+      expect(r.subraza, equals(["afghan","basset","blood","english","ibizan","plott","walker"]));
+    });
+  });
+  test('no existente', () {
+    var resultado = repositorio.obtenerRegistroRaza(RazaFormada.constructor('kek'));
+    resultado.match((l) {
+      expect(l, isA<RazaNoRegistrada>());
+    }, (r) {
+      
+    });
+  });
+ });
+}
