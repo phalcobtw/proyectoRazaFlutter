58524719af3a5fce7ae54587efd867cc11a6a7f6
phalcobtw <laxz_16@hotmail.com>
2022-10-05 23:48:04 -0600

    bloc sin terminar, para guardar avance

--- lib/bloc.dart
+++ lib/bloc.dart
@@ -1,12 +1,20 @@
+import 'package:dogapi/problemas.dart';
+import 'package:dogapi/raza_formada.dart';
+import 'package:dogapi/repositorio_verificacion.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EventoVerificacion{}
 
 class Creado extends EventoVerificacion{}
 
-class RazaRecibida extends EventoVerificacion{}
+class RazaRecibida extends EventoVerificacion{
+  final RazaFormada raza;
+  RazaRecibida(this.raza);
+}
 
 class RazaConfirmada extends EventoVerificacion{}
+
+
 
 
 class EstadoVerificacion{}
@@ -17,14 +25,32 @@ class SolicitandoRaza extends EstadoVerificacion{}
 
 class EsperandoConfirmacionRaza extends EstadoVerificacion{}
 
-class MostrarRaza extends EstadoVerificacion{}
+class MostrarRaza extends EstadoVerificacion{
 
-class MostrarRazaNoConfirmada extends EstadoVerificacion{}
+}
+
+class MostrarRazaNoConfirmada extends EstadoVerificacion{
+  final RazaFormada raza;
+  MostrarRazaNoConfirmada(this.raza);
+}
+class MostrarSinSubraza extends EstadoVerificacion{
+  final RazaFormada raza;
+  MostrarSinSubraza(this.raza);
+}
 
 class ClaseBloc extends Bloc<EventoVerificacion, EstadoVerificacion> {
-  ClaseBloc() : super((Creandose())) {
+  final RepositorioVerificacion _repositorioVerificacion;
+  ClaseBloc(this._repositorioVerificacion) : super((Creandose())) {
     on<Creado>((event, emit) {
       emit(SolicitandoRaza());
     });
+    on<RazaRecibida>((event, emit) {
+      emit(EsperandoConfirmacionRaza());
+      final resultado = _repositorioVerificacion.obtenerRegistroRaza(event.raza);
+      resultado.match((l) {
+        if (l is VersionIncorrectaJson) emit(MostrarErrorVersion());
+        if(l is RazaNoRegistrada) emit(MostrarRazaNoRegistrada());
+      }, (r) => null);
+    },);
   }
 }
