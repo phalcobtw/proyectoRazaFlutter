98062db72046c219c3e42743c3b215aafc180ca4
phalcobtw <laxz_16@hotmail.com>
2022-10-06 00:35:41 -0600

    bloc test al fin por dios

--- test/bloc_test.dart
+++ test/bloc_test.dart
@@ -1,11 +1,12 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:dogapi/bloc.dart';
+import 'package:dogapi/repositorio_verificacion.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   blocTest<ClaseBloc, EstadoVerificacion>(
     'emite el estado cuando sucede Creado',
-    build: () => ClaseBloc(),
+    build: () => ClaseBloc(RepositorioPruebaVerificacion()),
     act: (bloc) => bloc.add(Creado()),
     expect: () => const [isA<SolicitandoRaza>],
   );
